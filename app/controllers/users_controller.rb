ffee14edb88afa55351c08f7c0dda67d017a51cc
bargholz eimii <818_77_@ezweb.ne.jp>
2022-11-04 16:04:16 +0900

    新規登録、編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,37 @@
 class UsersController < ApplicationController
+  skip_before_action :login_required, only: [:new, :create, :edit, :update]
+  
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to user_path(@user.id)
+    redirect_to :new
+    end
+  end
+
+  
+  def edit
+    @user = User.find(params[:id])
+    unless @user.id = current_user.id
+      redirect_to  new_post_path
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path, notice: "編集しました！"
+    else
+      render :edit
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name,:email,:password,:password_confirmation)
+  end
 end
