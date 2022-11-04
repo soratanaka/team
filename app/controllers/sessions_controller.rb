dc430d393a66b6f6d1f479af3c9f2853e2144faa
田中宙 <tanakachuu@tanakachuunoAir.elecom>
2022-11-04 15:47:03 +0900

    session make

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,38 @@
 class SessionsController < ApplicationController
+  skip_before_action :login_required, only: [:new, :create]
+
   def new
+  end
+
+  def create
+    email = params[:session][:email].downcase
+    password = params[:session][:password]
+    if login(email, password)
+      p 'ログインしました'
+      #flash[:success] = 'ログインに成功しました。'
+      redirect_to new_session_path #user_path(@user.id)
+    else
+      p 'ログインに失敗しました'
+      #flash.now[:danger] = 'ログインに失敗しました。'
+      render 'new'
+    end
+  end
+  
+  def destroy
+    session.delete(:user_id)
+    flash[:notice] = 'ログアウトしました'
+    redirect_to new_session_path
+  end
+
+  private
+
+  def login(email, password)
+    @user = User.find_by(email: email)
+    if @user && @user.authenticate(password)
+      session[:user_id] = @user.id
+      return true
+    else
+      return false
+    end
   end
 end
